fb25dcd1e38e034ba59e03e6a2c41fb3c7c7d45a
Rick Broker <rbroker@xebialabs.com>
2017-09-28 07:58:17 -0400

    Move to xl-rules

--- src/main/resources/windowsFile/destroy-windows-file.ps1
+++ src/main/resources/windowsFile/destroy-windows-file.ps1
@@ -3,18 +3,19 @@ THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EIT
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
 FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
 #>
-
+$deployed = $previousDeployed
 if ( $deployed.targetFileName ) {
-     Write-Host "Setting TARGET_FILE_NAME to deployed.targetFileName"
+     Write-Host "Setting TARGET_FILE_NAME to " $deployed.targetFileName
      $TARGET_FILE_NAME = $deployed.targetFileName
 } else {
-     Write-Host "Setting TARGET_FILE_NAME to deployed.name"
-     $TARGET_FILE_NAME = $deployed.file
+     Write-Host "Setting TARGET_FILE_NAME to " $deployed.name
+     $TARGET_FILE_NAME = $deployed.name
 }
 
 $TARGET_PATH = $deployed.targetPath
-$TARGET_FILE = "$TARGET_PATH\$TARGET_FILE_NAME"
+$TARGET_FILE = $TARGET_PATH + "\" + $TARGET_FILE_NAME
 
+Write-Host "Setting TARGET_FILE to " $TARGET_FILE
 if ( (Test-Path $TARGET_FILE ) -eq $false ) {
      Write-Host "WARN: $TARGET_FILE not found. Nothing to do."
   exit
